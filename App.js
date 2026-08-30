ec6eb23de128d3fd540abb94df537dff0370a6b0
faivr <faivr@CLEMENT-DESKTOP>
2026-08-30 16:14:42 +0200

    :sparkles: added app

--- App.js
+++ App.js
@@ -3,47 +3,55 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { DatabaseProvider } from "./src/db/DatabaseContext";
 import LogStack from "./src/navigations/LogStacks";
 import ActivityScreen from "./src/screens/ActivityScreen";
-import ScannerScreen from "./src/screens/ScannerScreen";
-import DashboardScreen from "./src/screens/DashboardScreen";
+import DashboardStack from "./src/navigations/DashboardStacks";
 import ProfileStacks from "./src/navigations/ProfileStacks";
 import { Text } from "react-native";
+import {AchievementProvider} from "./src/contexts/AchievementContext"
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <DatabaseProvider>
-      <NavigationContainer>
+      <AchievementProvider>
+        <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen 
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{
-            tabBarIcon: () => <Text style={{fontSize:20}}>🏠</Text>
-          }} />
+            name="Dashboard"
+            component={DashboardStack}
+            options={{
+              headerShown: false, // Hides the duplicate header from the Tab Navigator
+              tabBarIcon: () => <Text style={{fontSize:20}}>🏠</Text>
+            }} 
+          />
 
           <Tab.Screen 
-          name="Journal" 
-          component={LogStack} 
-          options={{
-            tabBarIcon: () => <Text style={{fontSize:20}}>📔</Text>
-          }} />
+            name="Journal" 
+            component={LogStack} 
+            options={{
+              tabBarIcon: () => <Text style={{fontSize:20}}>📔</Text>
+            }} 
+          />
 
           <Tab.Screen 
-          name="Activité" 
-          component={ActivityScreen}
-          options={{
-            tabBarIcon: () => <Text style={{fontSize:20}}>🏋️</Text>
-          }}  />
+            name="Activité" 
+            component={ActivityScreen}
+            options={{
+              tabBarIcon: () => <Text style={{fontSize:20}}>🏋️</Text>
+            }}  
+          />
 
           <Tab.Screen 
-          name="Profile" 
-          component={ProfileStacks}
-          options={{
-            tabBarIcon: () => <Text style={{fontSize:20}}>👤</Text>
-          }}   />
+            name="Profile" 
+            component={ProfileStacks}
+            options={{
+              tabBarIcon: () => <Text style={{fontSize:20}}>👤</Text>
+            }}   
+          />
         </Tab.Navigator>
       </NavigationContainer>
+      </AchievementProvider>
+      
     </DatabaseProvider>
   );
 }
